dbdfa1076360e0b58025c0095a81cf82436846f0
TristanK117 <tristankhieu28@gmail.com>
2024-05-17 09:13:06 -0700

    added routing but an error with app

--- src/components/App.js
+++ src/components/App.js
@@ -2,17 +2,17 @@ import React from 'react';
 import {Routes, Route} from 'react-router-dom';
 // import Home from './Home';
 import { Messenger } from './Messenger';
-//import PostListing from './PostListing';
-// import UserListings from './UserListings';
+import { PostListing } from './PostListing';
+import { UserListings } from './UserListings';
 
 export default function App() {
   return (
     <div>
       <Routes>
         {/* <Home /> */}
-        <Messenger />
-        {/* <PostListing /> */}
-        {/* <UserListings /> */}
+        <Route path="Messanger" element={<Messenger />}/>
+        <Route path="PostListing" element={<PostListing />}/>
+        <Route path="UserListings" element={<UserListings />}/>
       </Routes>
     </div>
   );
