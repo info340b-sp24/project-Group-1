24ae27dd8865d121e8657dde2f867eeea9acf199
TristanK117 <tristankhieu28@gmail.com>
2024-06-01 17:06:01 -0700

    added auth and db to index

--- src/index.js
+++ src/index.js
@@ -7,6 +7,8 @@ import App from './components/App';
 // FIREBASE CONFIG
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getFirestore } from 'firebase/firestore';
+import { getAuth } from 'firebase/auth';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -22,6 +24,8 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
