495d1e99a67b67f48617f4504c856755b1ec0f35
Shaaz Charania <shaazch@uw.edu>
2024-06-03 15:49:35 -0700

    profile fetches information from firebase

--- src/components/EditProfileMenu.js
+++ src/components/EditProfileMenu.js
@@ -1,16 +1,33 @@
 import React, { useState } from 'react';
 import { getDatabase, ref, set } from 'firebase/database';
 
+const cities = [
+  'Univeristy District',
+  'Roosevelt',
+  'Green Lake',
+  'Ravenna',
+  'Wallingford',
+  'Laurelhurst',
+  'Northgate',
+  'Downtown Seattle',
+  'Shoreline',
+  'Ballard',
+  'Capitol Hill',
+  'Norht Beacon Hill',
+  'Mt. Baker',
+  'Queen Anne'
+];
+
 export default function EditProfileMenu({ currentUser, setCurrentUser, onProfileUpdate }) {
   const [username, setUsername] = useState(currentUser.userName);
   const [location, setLocation] = useState(currentUser.location || '');
   const [profileImg, setProfileImg] = useState(null);
 
   const handleSave = () => {
-    // if (!currentUser.id) {
-    //   console.error('User ID is undefined');
-    //   return;
-    // }
+    if (!currentUser.userId) {
+      console.error('User ID is undefined');
+      return;
+    }
 
     const updatedUser = {
       ...currentUser,
@@ -43,12 +60,15 @@ export default function EditProfileMenu({ currentUser, setCurrentUser, onProfile
         value={username}
         onChange={(e) => setUsername(e.target.value)}
       />
-      <input 
-        type="text"
-        placeholder="Location"
+      <select 
         value={location}
         onChange={(e) => setLocation(e.target.value)}
-      />
+      >
+        <option value="">Select City</option>
+        {cities.map((city, index) => (
+          <option key={index} value={city}>{city}</option>
+        ))}
+      </select>
       <input
         type="file"
         accept="image/*"
